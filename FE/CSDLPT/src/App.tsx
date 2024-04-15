5cbb06c6075e1d9d055d0b9c76c07b3175a045d4
SIU <90703662+NguyenNhatMinh-BYS@users.noreply.github.com>
2024-04-15 23:51:52 +0700

    setup Frontend

---
+++ FE/CSDLPT/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p className="text-[red]">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
