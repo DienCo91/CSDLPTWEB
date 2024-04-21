876d729a815ed1e0045e6aad302c2c9f03e13e30
SIU <90703662+NguyenNhatMinh-BYS@users.noreply.github.com>
2024-04-21 16:43:25 +0700

    clear comment

--- FE/CSDLPT/src/components/Nav/Nav.tsx
+++ FE/CSDLPT/src/components/Nav/Nav.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import subLogo from "../../assets/img/subLogo.png";
 import "./Nav.css";
 import { useLocation } from "react-router-dom";
--- FE/CSDLPT/src/page/home-page/Home.tsx
+++ FE/CSDLPT/src/page/home-page/Home.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import HomeImg from "../../assets/img/HomeImage.png";
 const POST = [
   {
